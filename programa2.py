d76bd43c9161f69e8b1860a7bb4940e25a222777
criz004 <50933375+criz004@users.noreply.github.com>
2025-05-19 13:36:14 -0600

    Update programa2.py

--- programa2.py
+++ programa2.py
@@ -40,6 +40,7 @@ def programa_2(display, touch, Boton):
                 x_inv, y = touch.get_coordinates()
                 x = WIDTH - x_inv
                 print(f"Toque detectado: ({x}, {y})")
+                time.sleep(5)
                 estado = 0  # Volver al menú principal
                 break
             except:
